78091427296f56903c1c7d3c79eb8adb17d50b1c
Yash Mehta <imyash@live.com>
2021-02-26 11:37:13 -0800

    chp 2 build

---
+++ app/src/main/java/com/example/geoquiz2/Question.java
@@ -0,0 +1,27 @@
+package com.example.geoquiz2;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question (int textResId, boolean answerTrue) {
+        mTextResId =  textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
