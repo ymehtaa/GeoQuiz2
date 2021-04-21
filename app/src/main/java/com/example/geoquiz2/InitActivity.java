e9251bdd4c4f6ab2364302ef793e46116b2d169b
Yash Mehta <imyash@live.com>
2021-04-21 09:22:28 -0700

    new main page, buttons created

---
+++ app/src/main/java/com/example/geoquiz2/InitActivity.java
@@ -0,0 +1,14 @@
+package com.example.geoquiz2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_init);
+    }
+}
